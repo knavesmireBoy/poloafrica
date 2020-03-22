fca055dc129ecc07bddc89959df5728548078f08
knavesmireBoy <andrewsykes@btinternet.com>
2020-03-22 17:45:13 +0000

    stack error in IE 9

--- js/finder.js
+++ js/finder.js
@@ -164,5 +164,10 @@
 				},
 		        */
 		float_handler;
-	
+	/* float is used for layout on older browsers and requires that the image comes before content in page source order
+	if flex is fully supported we can re-order through css. We provide a javascript fallback for browsers that don't support flex(wrap). If javascript is disabled we can use input/labels, but the picture will come before the content
+	*/
+	//main.addEventListener('click', bridge);
+    utils.addHandler('click', main, bridge);
+
 }(Modernizr.mq('only all'), '(min-width: 668px)'));
