ea48510c82051ef7ccd5a0e1a636f7d2317a6343
knavesmireBoy <andrewsykes@btinternet.com>
2021-01-06 13:39:15 +0000

    code improvements

--- js/s.js
+++ js/s.js
@@ -4,7 +4,7 @@
 /*global Modernizr: false */
 /*global poloAF: false */
 /*global _: false */
-(function (mq, query, touchevents, picnum, makePath) {
+(function (mq, query, touchevents, picnum, dummy, makePath) {
 	"use strict";
     
      function LoopIterator() {
@@ -141,37 +141,36 @@
 		getTarget = utils.drillDown([mytarget]),
         allpics = utils.getByTag('img', main),
 		getOrientation = ptL(compare, utils.gtThan, 'offsetHeight', 'offsetWidth'),
-		neg = function (a, b) {
+		
+		negator = (function () {
+           var neg = function (a, b) {
 			return getLength(b) !== 14;
-		},
-		negator = function (cb, a, b) {
-            console.log(a.length, b.length)
+		}; 
+            return function(cb, a, b){
 			if (neg(a, b)) {
-				cb.apply(null, _.rest(arguments, 3));
+                cb();
 				neg = _.negate(neg);
 			}
 			return b;
-		},
+		};
+        }()),
         doPortrait = function (el) {
 			var m = getOrientation(el) ? 'addClass' : 'removeClass';
 			utils[m]('portrait', utils.getDomParent(utils.getNodeByTag('li'))(el));
 		},
         
 		fixNoNthChild = _.compose(ptL(utils.doWhen, _.negate(utils.always(Modernizr.nthchild))), ptL(partial, doPortrait)),
-		doToggle = ptL(klasTog, 'alt', thumbs),
-		doPortraitLoop = ptL(_.each, allpics, fixNoNthChild),
-		doPortraitBridge = function (e) {
-			fixNoNthChild(e.target);
-		},
 		doPopulate = function (pagepics) {
 			var path = '001';
 			_.each(allpics, function (img, i) {
 				path = pagepics[i] || path; //? default to avoid null
 				img.src = makePath(path);
-				img.onload = doPortraitBridge;
+                //adds portrait class on browsers that don't support nth-child
+				img.onload = function (e) {
+                    fixNoNthChild(this);
+                }
 			});
 		},
-		toogleLoop = _.compose(doPortraitLoop, doToggle), //cb
 		$LI = (function (options) {
 			return {
 				exec: function () { //cb
@@ -180,7 +179,6 @@
 					options = options.reverse();
 				},
 				unrender: function (el) {
-                    console.log(el);
 					var $el = makeElement(always(el)).render();
 					$el.unrender();
 				},
@@ -194,13 +192,7 @@
             return prepIterator(ptL(modulo, coll.length))(utils.always(true))(coll)(0);
 		},
 		cross_page_iterator = makeCrossPageIterator(all),
-		populate = _.compose(doPopulate, ptL(negator, _.compose(toogleLoop, _.bind($LI.exec, $LI)), allpics)),
-        pageInputHandler = function (arg) {
-			//stopPropagation
-			//utils.addEvent(clicker, noOp, 'stop')(arg);
-            return eventing('click', noOp, arg, ['preventDefault', 'stopPropagation']);
-			//return arg;
-		},
+		populate = _.compose(doPopulate, ptL(negator, _.compose(ptL(klasTog, 'alt', thumbs), _.bind($LI.exec, $LI)), allpics)),
 		advanceRouteBridge = function (e) {
 			if (!getNodeName(getTarget(e)).match(/a/i)) {
 				return;
@@ -218,19 +210,22 @@
 				id: id,
 				href: '.'
 			}), myAnCr(main), utils.always('a'))))('span');
-          if(el) el.render();
+         //el.render();
             return el;
 		},
 		pageNavHandler = _.compose(ptL(eventing, 'click', null, _.debounce(advanceRouteListener, 300)), utils.getDomParent(utils.getNodeByTag('main'))),
-		insertBeforeThumbs = ptL(anCrIn, gallery);
-	addPageNav(anCr, 'gal_forward', noOp);
-	//addPageNavHandler delegates to main as the listening element, only one handler required, hence noOp above
-	//addPageNav(insertBeforeThumbs, 'Enable checkbox to group pictures by orientation', 'gal_back', addPageNavHandler);
-	addPageNav(insertBeforeThumbs, 'gal_back', pageNavHandler);
-    
+        $nav = addPageNav(ptL(anCrIn, gallery), 'gal_back', pageNavHandler);
+        addPageNav(anCr, 'gal_forward', function(){ return dummy; });
+    $nav.render();
+    _.each(allpics, fixNoNthChild);
 	utils.$('placeholder').innerHTML = 'PHOTOS';
     
-}(Modernizr.mq('only all'), '(min-width: 668px)', Modernizr.touchevents, new RegExp('[^\\d]+\\d(\\d+)[^\\d]+$'), function (path) {
+}(Modernizr.mq('only all'), '(min-width: 668px)', Modernizr.touchevents, new RegExp('[^\\d]+\\d(\\d+)[^\\d]+$'), {
+            render: function(){},
+            unrender: function(){}
+        }, function (path) {
 	"use strict";
-	return "images/0" + path + ".jpg";
+    path = ''+path;
+    var mypath = path.length < 3 ? "images/0"+path : "images/"+path;
+	return mypath + ".jpg";
 }));
