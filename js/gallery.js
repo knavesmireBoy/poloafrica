0e7b58fd957e5d19a89449b60627456fd71e36ee
knavesmireBoy <andrewsykes@btinternet.com>
2021-01-22 17:36:01 +0000

    trigger

--- js/gallery.js
+++ js/gallery.js
@@ -817,6 +817,7 @@
 	$nav.render();
 	_.each(allpics, fixNoNthChild);
     utils.$('placeholder').innerHTML = 'PHOTOS';
+    triggerEvent(document.images[2], 'click');
 }(Modernizr.mq('only all'), '(min-width: 668px)', Modernizr.touchevents, '../images/resource/', new RegExp('[^\\d]+\\d(\\d+)[^\\d]+$'), {
 	render: function () {
 		"use strict";
