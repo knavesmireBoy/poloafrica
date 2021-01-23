7efbd56c0df46db73960373d45342e96f58d244a
knavesmireBoy <andrewsykes@btinternet.com>
2021-01-23 09:11:59 +0000

    prevdefault test

--- js/gallery.js
+++ js/gallery.js
@@ -801,7 +801,7 @@
            // utils.$('placeholder').innerHTML = 'bliss';
   try{
 			_.compose(setindex, utils.drillDown([mytarget, 'src']))(e);
-			_.compose(thrice(doMapBridge)('className')('static'), thrice(doMapBridge)('id')('controls'), anCr(main))('section');
+			_.compose(utils.addClass('static'), thrice(doMapBridge)('id')('controls'), anCr(main))('section');
            
             //doMakeBase(e.target.src, 'base', doOrient(unsetPortrait, setPortrait), getBaseChild, showtime);
   }
